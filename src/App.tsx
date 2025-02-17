c37582fac8dff925d1ad17f2f92e7f092f2ddb28
kshitij <kpandey0022@gmail.com>
2025-02-17 23:52:08 +0530

    Added Announcement and Events

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './components/Landing';
 import Home from './components/Home';
 import Announcement from './components/Announcement';
-import Emergency from './components/Emergency/Emergency'
+import Emergency from './components/Emergency/Emergency';
+import Events from './components/Events'; // Import Events component
 
 function App() {
   return (
@@ -10,11 +11,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/Announcement" element={<Announcement />} />
-        <Route path="/Emergency" element={<Emergency />} />
-
-        {/* Add other routes here */}
+        <Route path="/announcements" element={<Announcement />} /> {/* Changed to lowercase */}
+        <Route path="/emergency" element={<Emergency />} />
+        <Route path="/events" element={<Events />} /> {/* Changed to lowercase */}
       </Routes>
     </Router>
   );
